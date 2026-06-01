892a48bb77c032c25eb916fc46a0e4843fca96ca
Isha-11032004 <ishac687@gmail.com>
2026-06-01 14:21:08 +0530

    Use bash build script for deployment

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+echo "=== BUILDING BACKEND ==="
+npm install --legacy-peer-deps --prefix server
+
+echo "=== BUILDING FRONTEND ==="
+npm install --legacy-peer-deps --prefix client
+npm run build --prefix client
+
+echo "=== BUILD COMPLETE ==="
